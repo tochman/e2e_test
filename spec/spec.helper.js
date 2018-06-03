71a3a6f4258ada88ea7106e2491b623f9aff12c5
tochman <thomas@craftacademy.se>
2018-06-03 21:38:58 +0200

    adds comments to spec.helper

--- spec/spec.helper.js
+++ spec/spec.helper.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+// Read all js files you want to test
 let calc = fs.readFileSync('./src/js/calculator.js');
 let person = fs.readFileSync('./src/js/person.js');
+// create modules from all js files
 eval(calc + person + `\nexports.Person = Person;\nexports.BMICalculator = BMICalculator;`)
